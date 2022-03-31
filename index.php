85a0be396a5086699e4e31784b182e109b4b2a97
derivescript <danielcfaria@gmail.com>
2022-03-31 16:26:44 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/*
+|---------------------------------------------------------
+| S.I.L.A.S - Um framework legal
+| Autor: Daniel da Costa e Faria - danielcfaria@gmail.com
+|---------------------------------------------------------
+*/
+
+
+/*
+|---------------------------------------------------------
+| Começando os trabalhos
+|---------------------------------------------------------
+*/
+ini_set('display_errors','on');
+ini_set('error_reporting', E_ALL);
+define('br','<br>');
+define('sisdir','sistema');
+define('appdir','apps');
+define('modeldir',appdir.'/models/');
+require 'core/Silas.php';
